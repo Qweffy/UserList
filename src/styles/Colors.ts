620a68324bc191d139ae00f70bdf58ef6fefc4d2
Niko Mastakas <nicolasmastakas@gmail.com>
2024-04-03 16:58:08 -0300

    feat: add design system

---
+++ src/styles/Colors.ts
@@ -0,0 +1,23 @@
+const Primary = '#FF3B30' // Rojo para botones y elementos destacados
+const PrimaryLighter = '#FFD1CF' // Rojo claro para fondos o resaltados suaves
+const Secondary = '#34C759' // Verde para notificaciones o estados de éxito
+const SecondaryLighter = '#DDFFE6' // Verde claro para fondos o resaltados suaves
+const Background = '#FFFFFF' // Blanco para fondos de pantallas y tarjetas
+const CardShadow = '#E5E5E5' // is claro para sombras o separadores
+const Black = '#000000' // Neo para texto principal
+const TextSecondary = '#8E8E93' // is para texto secundario
+const Icon = '#AEAEB2' // is para iconos o elementos no resaltados
+const InputBackground = '#F2F2F7' // is claro para fondos de campos de entrada
+
+export const Colors = {
+  Primary,
+  PrimaryLighter,
+  Secondary,
+  SecondaryLighter,
+  Background,
+  CardShadow,
+  Black,
+  TextSecondary,
+  Icon,
+  InputBackground,
+}
